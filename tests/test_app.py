259a4761926a758e523a3f1801083713c37208a1
DarekW90 <walda.darek@gmail.com>
2025-01-10 17:22:16 +0100

    removing polish letters

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,31 +7,31 @@ import requests
 class FlaskRoutesTestCase(unittest.TestCase):
     """Klasa testowa dla testowania tras w aplikacji Flask.
     Metody testowe:
-    - test_check_server(): Sprawdza, czy serwer jest dostępny.
-    - test_landing_page(): Testuje stronę główną aplikacji.
-    - test_measurements_page(): Testuje stronę z pomiarami.
-    - test_aquarium_page(): Testuje stronę z Akwarium.
-    - test_air_quality_page(): Testuje stronę dotyczącą jakości powietrza.
-    - test_camera_page(): Testuje stronę z kamerą.
-    Wszystkie metody testowe wykonują zapytania HTTP na odpowiednie adresy URL serwera
-    i sprawdzają status odpowiedzi. Jeśli status jest inny niż 200, test kończy się niepowodzeniem.
-    W przypadku błędu połączenia z serwerem, test również kończy się niepowodzeniem.
-    - AssertionError: Jeśli status odpowiedzi jest inny niż 200.
-    - self.fail: Jeśli nie można połączyć się z serwerem.
-    :param unittest.TestCase: Klasa bazowa dla testów jednostkowych"""
+    - test_check_server(): Sprawdza, czy serwer jest dostepny.
+    - test_landing_page(): Testuje strone glowna aplikacji.
+    - test_measurements_page(): Testuje strone z pomiarami.
+    - test_aquarium_page(): Testuje strone z Akwarium.
+    - test_air_quality_page(): Testuje strone dotyczaca jakosci powietrza.
+    - test_camera_page(): Testuje strone z kamera.
+    Wszystkie metody testowe wykonuja zapytania HTTP na odpowiednie adresy URL serwera
+    i sprawdzaja status odpowiedzi. Jesli status jest inny niz 200, test konczy sie niepowodzeniem.
+    W przypadku bledu polaczenia z serwerem, test rowniez konczy sie niepowodzeniem.
+    - AssertionError: Jesli status odpowiedzi jest inny niz 200.
+    - self.fail: Jesli nie mozna polaczyc sie z serwerem.
+    :param unittest.TestCase: Klasa bazowa dla testow jednostkowych"""
 
     BASE_URL = "http://127.0.0.1:5000"
 
     def test_check_server(self):
         """
-        Sprawdza, czy serwer jest dostępny.
+        Sprawdza, czy serwer jest dostepny.
         Metoda wykonuje zapytanie GET na podstawowy adres URL serwera.
-        Jeśli odpowiedź serwera ma kod innny niż 200, wypisuje
+        Jesli odpowiedz serwera ma kod innny niz 200, wypisuje
         komunikat "Server offline".
         W przeciwnym razie wypisuje komunikat "Server is running!".
-        Jeśli wystąpi błąd podczas wykonywania zapytania,
-        metoda kończy się niepowodzeniem
-        i wypisuje komunikat z informacją o błędzie.
+        Jesli wystapi blad podczas wykonywania zapytania,
+        metoda konczy sie niepowodzeniem
+        i wypisuje komunikat z informacja o bledzie.
         :param self: Obiekt testowy
         :return: None
         """
@@ -47,13 +47,13 @@ class FlaskRoutesTestCase(unittest.TestCase):
 
     def test_landing_page(self):
         """
-        Testuje stronę główną aplikacji.
-        Sprawdza, czy można poprawnie nawiązać połączenie
-        z serwerem i czy otrzymano odpowiedź o kodzie 200.
-        Jeśli nie można nawiązać połączenia,
-        test kończy się niepowodzeniem.
+        Testuje strone glowna aplikacji.
+        Sprawdza, czy mozna poprawnie nawiazac polaczenie
+        z serwerem i czy otrzymano odpowiedz o kodzie 200.
+        Jesli nie mozna nawiazac polaczenia,
+        test konczy sie niepowodzeniem.
         Raises:
-            AssertionError: Jeśli nie otrzymano
+            AssertionError: Jesli nie otrzymano
             odpowiedzi o kodzie 200.
         """
 
@@ -66,15 +66,15 @@ class FlaskRoutesTestCase(unittest.TestCase):
 
     def test_measurements_page(self):
         """
-        Testuje stronę z pomiarami.
-        Sprawdza, czy można poprawnie pobrać stronę z pomiarami z serwera.
-        Jeśli połączenie z serwerem nie powiedzie się,
-        test zostanie zakończony niepowodzeniem.
+        Testuje strone z pomiarami.
+        Sprawdza, czy mozna poprawnie pobrac strone z pomiarami z serwera.
+        Jesli polaczenie z serwerem nie powiedzie sie,
+        test zostanie zakonczony niepowodzeniem.
         Raises:
-            AssertionError: Jeśli status odpowiedzi
-            nie jest równy 200.
+            AssertionError: Jesli status odpowiedzi
+            nie jest rowny 200.
         """
-        self.fail("Nie można połączyć się z serwerem: Measurements Page")
+        self.fail("Nie mozna polaczyc sie z serwerem: Measurements Page")
 
         try:
             response = requests.get(f"{self.BASE_URL}/measurements_page")
@@ -85,14 +85,14 @@ class FlaskRoutesTestCase(unittest.TestCase):
 
     def test_aquarium_page(self):
         """
-        Testuje stronę z Akwarium.
-        Sprawdza, czy można poprawnie pobrać stronę z Akwarium z serwera.
-        Jeśli połączenie z serwerem jest udane i status odpowiedzi wynosi 200,
+        Testuje strone z Akwarium.
+        Sprawdza, czy mozna poprawnie pobrac strone z Akwarium z serwera.
+        Jesli polaczenie z serwerem jest udane i status odpowiedzi wynosi 200,
         wypisuje komunikat "Test response: dla strony z Akwarium - OK".
-        Jeśli wystąpi błąd połączenia, test kończy się niepowodzeniem
-        i wypisuje komunikat "Nie można połączyć się z serwerem: Akwarium".
+        Jesli wystapi blad polaczenia, test konczy sie niepowodzeniem
+        i wypisuje komunikat "Nie mozna polaczyc sie z serwerem: Akwarium".
         """
-        self.fail("Nie można połączyć się z serwerem: Akwarium")
+        self.fail("Nie mozna polaczyc sie z serwerem: Akwarium")
 
         try:
             response = requests.get(f"{self.BASE_URL}/aquarium")
@@ -103,34 +103,34 @@ class FlaskRoutesTestCase(unittest.TestCase):
 
     def test_air_quality_page(self):
         """
-        Testuje stronę dotyczącą jakości powietrza.
-        Sprawdza, czy można pobrać odpowiedź z serwera
+        Testuje strone dotyczaca jakosci powietrza.
+        Sprawdza, czy mozna pobrac odpowiedz z serwera
         dla strony o adresie {self.BASE_URL}/air_quality.
         Oczekiwany kod odpowiedzi to 200.
-        Jeśli nie można połączyć się z serwerem,
-        test kończy się niepowodzeniem.
+        Jesli nie mozna polaczyc sie z serwerem,
+        test konczy sie niepowodzeniem.
         Raises:
-            AssertionError: Jeśli kod odpowiedzi jest inny niż 200.
-            self.fail: Jeśli nie można połączyć się z serwerem.
+            AssertionError: Jesli kod odpowiedzi jest inny niz 200.
+            self.fail: Jesli nie mozna polaczyc sie z serwerem.
         """
-        self.fail("Nie można połączyć się z serwerem: Jakości Powietrza")
+        self.fail("Nie mozna polaczyc sie z serwerem: Jakosci Powietrza")
 
         try:
             response = requests.get(f"{self.BASE_URL}/air_quality")
             self.assertEqual(response.status_code, 200)
             print("Test response: dla strony z Jakosci Powietrza - OK")
         except requests.exceptions.ConnectionError:
-            self.fail("Nie mozna polaczyc sie z serwerem: Jakości Powietrza")
+            self.fail("Nie mozna polaczyc sie z serwerem: Jakosci Powietrza")
 
     def test_camera_page(self):
         """
-        Testuje stronę z kamerą.
-        Sprawdza, czy można poprawnie połączyć się
-        z serwerem i otrzymać odpowiedź o kodzie 200.
-        Jeśli nie można połączyć się z serwerem,
-        test kończy się niepowodzeniem.
+        Testuje strone z kamera.
+        Sprawdza, czy mozna poprawnie polaczyc sie
+        z serwerem i otrzymac odpowiedz o kodzie 200.
+        Jesli nie mozna polaczyc sie z serwerem,
+        test konczy sie niepowodzeniem.
         """
-        self.fail("Nie można połączyć się z serwerem: Kamera")
+        self.fail("Nie mozna polaczyc sie z serwerem: Kamera")
 
         try:
             response = requests.get(f"{self.BASE_URL}/door_bell_page")
