40341a9d5d78097de8494b8104955fc52e3e2872
DarekW90 <walda.darek@gmail.com>
2025-01-10 22:13:25 +0100

    fast api refactor

--- fast_api_app.py
+++ fast_api_app.py
@@ -9,11 +9,11 @@ import os
 import time
 import cv2
 import socketio
-from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 
 # Konfiguracja FastAPI
 app=FastAPI()
